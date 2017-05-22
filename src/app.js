7e9cd75805870ab100c89c8abfbac5ee6337f20d
Mitch Gillin <mtg4ra@virginia.edu>
2017-05-22 09:02:25 -0400

    Project Push
    Not completed by a long shot. I tried to follow a tutorial but got
    bogged down in routing. There was an issue with interpreting a filter
    somewhere that I couldn’t troubleshoot.

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'font-awesome/css/font-awesome.css'
+import './app.css'
+
+import {browserHistory, Router, Route} from 'react-router'
+
+import App from 'containers/App/App'
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(
+  <App history={browserHistory} />, mountNode);
+
+const Home = React.createClass({
+  render: function() {
+    return (<div>Hello world</div>)
+  }
+})
+
+const routes = (
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>
+)
+
---
+++ src/containers/App/App.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+import { Router } from 'react-router';
+
+class App extends React.Component {
+  static propTypes = {
+    routes: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  }
+
+  // class getter
+  get content() {
+    return (<Router
+        routes={this.props.routes}
+        history={this.props.history} />)
+  }
+
+  render() {
+    return (
+      <div style={ { height: '100%' } }>
+        {this.content}
+      </div>
+    )
+  }
+}
